95a8f4af0f5a257f15b5b4f4e72ac90cb9d28796
Xtremind <guillaume.dagraca@gmail.com>
2019-01-28 11:44:03 +0000

    add date in log

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ var express = require('express');
 var app = express();
 
 app.listen(8080, () => {
-    console.log("Server running on port 8080");
+    console.log(now() + " Server running on port 8080");
 }),
 
 app.get("/status", (req, res, next) =>{
-    console.log("GET /status");
+    console.log(now() + " GET /status");
     res.json({"status": "ok"});
 });
 
 app.get("/users", (req, res, next) => {
     var id = req.param('id');
     if (typeof id === 'undefined'){
-        console.log("GET /users ");
+        console.log(now() + " GET /users ");
         res.json([{"id" : 1, "name" : "name1"},{"id" : 2, "name" : "name2"}]);
     }else {
-        console.log("GET /users : " + id);
+        console.log(now() + " GET /users : " + id);
         res.json({"id" : id, "name" : "name"+id});
     }
 });
@@ -24,15 +24,20 @@ app.get("/users", (req, res, next) => {
 app.get("/address", (req, res, next) => {
     var id = req.param('id');
     if (typeof id === 'undefined'){
-        console.log("GET /address : " + id);
+        console.log(now() + " GET /address : " + id);
         res.json({"error":"error"});
     }else {
-        console.log("GET /address : " + id);
+        console.log(now() + " GET /address : " + id);
         res.json({"id" : id, "address" : "address"+id});
     }
 });
 
 app.post("/test", (req, res, next) =>{
-    console.log("POST /test : " + req);
-    res.json({"status": "ok"});
+    console.log(now + " POST /test : " + req);
+    res.json({"test": "ok"});
 });
+
+function now(){
+    var now = new Date();
+    return now.toString();
+};
